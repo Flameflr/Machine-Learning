c9e6eb53d56d6ad021e58183eb0b688db283d761
Yannick <yannickgetto47@gmail.com>
2026-09-17 16:10:59 +0200

    added explaination and corrected typo in program

--- LinearRegression/AutoMPG/main.cpp
+++ LinearRegression/AutoMPG/main.cpp
@@ -16,16 +16,16 @@ int main() {
 	return -1;
     }
 
-    vector<double> Gpa;
+    vector<double> Mpg;
     vector<double> Weight;
 
-    double gpa;
+    double mpg;
     double weight;
     string temp;
 
-    while (file >> gpa >> temp >> temp >> temp >> weight >> temp >> temp >> temp >> temp) {
+    while (file >> mpg >> temp >> temp >> temp >> weight >> temp >> temp >> temp >> temp) {
 
-	Gpa.push_back(gpa);
+	Mpg.push_back(mpg);
 	Weight.push_back(weight);
 	getline(file, temp);
     }
@@ -33,30 +33,30 @@ int main() {
     double b0 = 0;
     double b1 = 0;
 
-    double gpa_sum = 0;
+    double mpg_sum = 0;
     double weight_sum = 0;
 
-    // for (auto &a : Gpa) cout << a << ' ';
+    // for (auto &a : Mpg) cout << a << ' ';
     // cout << '\n';
     // for (auto &a : Weight) cout << a << ' ';
     // cout << '\n';
 
 
-    for (auto &a : Gpa) gpa_sum += a;
-    gpa_sum = gpa_sum / Gpa.size();
+    for (auto &a : Mpg) mpg_sum += a;
+    mpg_sum = mpg_sum / Mpg.size();
     for (auto &a : Weight) weight_sum += a;
     weight_sum = weight_sum / Weight.size();
 
     double b1_bottom = 0;
-    for (long unsigned int i = 0; i < Gpa.size(); i++) {
+    for (long unsigned int i = 0; i < Mpg.size(); i++) {
 
-	b1 += (Weight[i] - weight_sum)*(Gpa[i] - gpa_sum);
+	b1 += (Weight[i] - weight_sum)*(Mpg[i] - mpg_sum);
 	b1_bottom += pow(Weight[i] - weight_sum, 2);
     }
 
     b1 = b1 / b1_bottom;
 
-    b0 = gpa_sum - b1*weight_sum;
+    b0 = mpg_sum - b1*weight_sum;
     
     double x = 4354;
     cout << b0 + b1*x;
